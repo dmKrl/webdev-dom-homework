8763a5eb84375aaa645746f9faf2e3d75da4a9c2
Dmitry Kralichkin <dm_krl@mail.ru>
2023-07-01 15:29:25 +0400

    some change

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const usersComments = [
   {
@@ -7,8 +7,7 @@ const usersComments = [
     comment: 'Это будет первый комментарий на этой странице',
     isLike: false,
     count: 3,
-    isEdit: false
-
+    isEdit: false,
   },
   {
     name: 'Варвара Н.',
@@ -16,14 +15,12 @@ const usersComments = [
     comment: 'Мне нравится как оформлена эта страница! ❤',
     isLike: true,
     count: 75,
-    isEdit: false
-  }
-]
-
-
+    isEdit: false,
+  },
+];
 
 const form = document.querySelector('.add-form');
-const formInputs = document.querySelector('.add-form-input')
+const formInputs = document.querySelector('.add-form-input');
 const inputAddNameForm = document.querySelector('.add-form-name');
 const areaAddFormRow = document.querySelector('.add-form-text');
 
@@ -38,8 +35,7 @@ const myDate = new Date();
 // textarea.classList.add('area-edit');
 // console.log(textarea)
 
-const arrayInputs = [inputAddNameForm, areaAddFormRow]
-
+const arrayInputs = [inputAddNameForm, areaAddFormRow];
 
 // Функция даты, для добавления значения 0 перед числами < 10
 const dateForComments = (date) => {
@@ -49,21 +45,22 @@ const dateForComments = (date) => {
   let minute = myDate.getMinutes();
 
   if (data < 10) {
-    data = "0" + data;
+    data = '0' + data;
   }
   if (month < 10) {
-    month = "0" + (month + 1);
+    month = '0' + (month + 1);
   }
   if (hour < 10) {
-    hour = "0" + hour;
+    hour = '0' + hour;
   }
   if (minute < 10) {
-    minute = "0" + minute;
+    minute = '0' + minute;
   }
-  return `${data}.${month}.${myDate.getFullYear().toString().substr(-2)} ${hour}:${minute}`;
-}
-
-
+  return `${data}.${month}.${myDate
+    .getFullYear()
+    .toString()
+    .substr(-2)} ${hour}:${minute}`;
+};
 
 // Функция рендера списка комментариев
 const renderComments = () => {
@@ -79,23 +76,31 @@ const renderComments = () => {
         <div class="comment-area edit-hidden" data-hidden="${!user.isEdit}">
          <textarea class="comment-area-edit" type="textarea" name="edit-text "data-index="${index}">фвывфы</textarea>
         </div>
-        <div class="comment-text edit-hidden" style="white-space: pre-line" data-hidden="${user.isEdit}" data-index="${index}">
+        <div class="comment-text edit-hidden" style="white-space: pre-line" data-hidden="${
+          user.isEdit
+        }" data-index="${index}">
           ${user.comment}
         </div>
       </div>
       <div class="comment-footer">
         <div class="likes">
-          <span class="likes-counter" data-index="${index}" data-count="${user.count}">${user.count}</span>
-          <button class="like-button" data-index="${index}" data-active-like="${user.isLike}"></button>
+          <span class="likes-counter" data-index="${index}" data-count="${
+        user.count
+      }">${user.count}</span>
+          <button class="like-button" data-index="${index}" data-active-like="${
+        user.isLike
+      }"></button>
         </div>
       </div>
       <div class="edit">
-      <button class="edit-button edit-hidden" data-index="${index}" data-hidden="${user.isEdit}">Редактировать</button>
+      <button class="edit-button edit-hidden" data-index="${index}" data-hidden="${
+        user.isEdit
+      }">Редактировать</button>
       <button class="edit-button-save edit-hidden" data-index="${index}" data-hidden="${!user.isEdit}">Сохранить</button>
     </div>
 
 
-    </li>`
+    </li>`;
     })
     .join('');
 
@@ -110,10 +115,8 @@ const renderComments = () => {
 
   // initAddLikesAndEditButtonListener()
   initAddLikesListenerForEnter();
-}
+};
 renderComments();
-
-
 
 // Создаём фунцию-генератор карточек
 function appendComment(userName, userComment, userDate) {
@@ -124,17 +127,14 @@ function appendComment(userName, userComment, userDate) {
     isLike: false,
     count: 0,
     isEdit: false,
-
-  })
+  });
 
   inputAddNameForm.value = '';
   areaAddFormRow.value = '';
 
   renderComments();
 }
-console.log(typeof usersComments)
-
-
+console.log(typeof usersComments);
 
 // Валидация
 function validateForm() {
@@ -146,43 +146,34 @@ function validateForm() {
   }
 }
 
-
-
 // Функция отключения активности кнопки при пустом input
 function handleInput() {
-  buttonAddForm.disabled = arrayInputs.some(input => !input.value.length);
+  buttonAddForm.disabled = arrayInputs.some((input) => !input.value.length);
 }
-
 
 // Вынесли отдельно код для обработчика событий
 const handleFormSubmission = () => {
   const inputValue = inputAddNameForm.value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;');
 
   const areaFormValue = areaAddFormRow.value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("QUOTE_BEGIN", "<div class='quote'>")
-    .replaceAll("QUOTE_END", "</div>")
-
-
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll('QUOTE_BEGIN', "<div class='quote'>")
+    .replaceAll('QUOTE_END', '</div>');
 
   inputValue === '' || areaFormValue === ''
     ? validateForm()
     : appendComment(inputValue, areaFormValue, dateForComments(myDate));
-}
-
-
+};
 
 // Вешаем обработчик событий на кнопку add comment
 buttonAddForm.addEventListener('click', handleFormSubmission);
-
-
 
 // Вешаем обработчик событий на клавишу enter
 function initAddLikesListenerForEnter() {
@@ -190,23 +181,19 @@ function initAddLikesListenerForEnter() {
     if (event.code === 'Enter') {
       handleFormSubmission();
     }
-  })
+  });
 }
-
-
 
 // Вешаем обработчик событий на кнопку delete comment
 buttonDeleteForm.addEventListener('click', () => {
   const liEnd = ulComments.children[ulComments.children.length - 1];
   console.log(liEnd.remove());
-})
-
-
+});
 
 // Функция изменения кнопки лайка и редактирования комментария
 function initAddLikesAndEditButtonListener() {
-  const textCommentsEditArea = document.querySelectorAll('.comment-area-edit')
-  const arrCommentsEditArea = Array.from(textCommentsEditArea)
+  const textCommentsEditArea = document.querySelectorAll('.comment-area-edit');
+  const arrCommentsEditArea = Array.from(textCommentsEditArea);
 
   // Слушатель события на список, поиск тригера(делегирование)
   ulComments.addEventListener('click', function (event) {
@@ -219,40 +206,37 @@ function initAddLikesAndEditButtonListener() {
       if (usersComments[index].isLike === false) {
         usersComments[index].isLike = true;
         usersComments[index].count++;
-        console.log(usersComments[index].isLike)
-
+        console.log(usersComments[index].isLike);
       } else {
         usersComments[index].isLike = false;
         usersComments[index].count--;
       }
+      renderComments();
     }
 
     // Проверка на таргет кнопки редактировать и сохранить
     if (target.closest('.edit-button') || target.closest('.edit-button-save')) {
-      console.log(usersComments[index].isEdit)
+      console.log(usersComments[index].isEdit);
 
       // Изменение кнопки в зависимости от состояния inputa
       if (usersComments[index].isEdit === false) {
-
-        console.log(usersComments[index])
+        console.log(usersComments[index]);
         usersComments[index].isEdit = true;
-
       } else {
-        console.log(usersComments[index].comment)
+        console.log(usersComments[index].comment);
         usersComments[index].isEdit = false;
         usersComments[index].comment = arrCommentsEditArea[index].innerText;
       }
+      renderComments();
     }
-
 
     // Реализация ответа на комментарий
     if (target.closest('.comment-text')) {
-      console.log(lastComment[index])
+      console.log(lastComment[index]);
       areaAddFormRow.value = `QUOTE_BEGIN > ${usersComments[index].name}
-  ${usersComments[index].comment} QUOTE_END`
+  ${usersComments[index].comment} QUOTE_END`;
+      renderComments();
     }
-    renderComments();
-  })
+  });
 }
-
-initAddLikesAndEditButtonListener()
+initAddLikesAndEditButtonListener();
